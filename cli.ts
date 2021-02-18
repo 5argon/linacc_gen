0acec8409d67756ce3aa4b4b87be22a7d1c35c8d
5argon <pyasry@gmail.com>
2021-02-18 18:07:00 +0700

    wrap cli things in import.meta.main

--- cli.ts
+++ cli.ts
@@ -3,26 +3,28 @@ import type { FlagsArgs } from "./deps.ts";
 import { createLinearAccessorFileFromFile } from "./generate.ts";
 import { SupportedInput, SupportedOutput } from "./supported.ts";
 
-const flags: FlagsArgs = parseFlags(Deno.args);
-const files: (string | number)[] = flags._;
+if (import.meta.main) {
+  const flags: FlagsArgs = parseFlags(Deno.args);
+  const files: (string | number)[] = flags._;
 
-if (files.length !== 2) {
-  throw new Error(
-    "Must specify exactly one input file, and one output file path."
+  if (files.length !== 2) {
+    throw new Error(
+      "Must specify exactly one input file, and one output file path."
+    );
+  }
+
+  const input = files[0];
+  const output = files[1];
+
+  if (typeof input !== "string" || typeof output !== "string") {
+    throw new Error("Input and output files must be string.");
+  }
+
+  createLinearAccessorFileFromFile(
+    input,
+    output,
+    SupportedInput.Yaml,
+    SupportedOutput.TypeScript,
+    "."
   );
 }
-
-const input = files[0];
-const output = files[1];
-
-if (typeof input !== "string" || typeof output !== "string") {
-  throw new Error("Input and output files must be string.");
-}
-
-createLinearAccessorFileFromFile(
-  input,
-  output,
-  SupportedInput.Yaml,
-  SupportedOutput.TypeScript,
-  "."
-);
